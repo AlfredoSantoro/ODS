e17aaea78e47299ced0b30988e825b5c938ec10d
Alfredo Santoro <alfredosantoro.informatico@gmail.com>
2021-08-03 19:19:31 +0200

    import ODS project on IBM CLOUD

--- src/main/kotlin/com/unisa/sesalab/ods/model/User.kt
+++ src/main/kotlin/com/unisa/sesalab/ods/model/User.kt
@@ -16,15 +16,20 @@ class User(
         @Column(nullable = false)
         val email: String,
         @Column(nullable = false)
-        val validUntil: LocalDate
+        val validUntil: LocalDate,
+        @Column(nullable = false)
+        val username: String,
+        @Column(nullable = false)
+        val password: String
 )
 {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     val id: Long = -1
-
     override fun toString(): String
     {
-        return "User(name='${this.name}', surname='${this.surname}', id=${this.id})"
+        return "User(name='$name', surname='$surname', userType=$userType, email='$email', validUntil=$validUntil, " +
+                "username='$username', password='$password', id=$id)"
     }
+
 }
