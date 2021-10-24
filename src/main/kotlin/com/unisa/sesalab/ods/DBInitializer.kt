a575bb9f25613e251c5f96f7ac03659f663693af
Alfredo Santoro <alfredosantoro.informatico@gmail.com>
2021-10-24 10:24:50 +0200

    v0.2.4 DB2 updated and added initSettings method

--- src/main/kotlin/com/unisa/sesalab/ods/DBInitializer.kt
+++ src/main/kotlin/com/unisa/sesalab/ods/DBInitializer.kt
@@ -1,10 +1,12 @@
 package com.unisa.sesalab.ods
 
 import com.unisa.sesalab.ods.dto.SeatInsertDTO
+import com.unisa.sesalab.ods.dto.SettingDTO
 import com.unisa.sesalab.ods.dto.TagNfcDTO
 import com.unisa.sesalab.ods.model.StudySeat
 import com.unisa.sesalab.ods.model.TagNfc
 import com.unisa.sesalab.ods.service.seat.SeatService
+import com.unisa.sesalab.ods.service.setting.SettingService
 import com.unisa.sesalab.ods.service.tagnfc.TagNFCService
 import com.unisa.sesalab.ods.service.user.UserService
 import development.kit.user.AccountType
@@ -15,6 +17,7 @@ import org.springframework.beans.factory.annotation.Value
 import org.springframework.boot.CommandLineRunner
 import org.springframework.stereotype.Component
 import org.springframework.transaction.annotation.Transactional
+import java.time.temporal.ChronoUnit
 import java.util.*
 
 @Component
@@ -22,10 +25,16 @@ class DBInitializer(
     private val userService: UserService,
     private val seatsService: SeatService,
     private val tagNFCService: TagNFCService,
+    private val settingService: SettingService,
     @Value("\${spring.jpa.hibernate.ddl-auto}")
     private val dbInitType: String
 ): CommandLineRunner
 {
+    @Value("\${settings.default-names.reservationHistory}")
+    private lateinit var reservationHistorySetting: String
+
+    private val days = 7
+
     private val logger: Logger = LoggerFactory.getLogger(DBInitializer::class.java)
 
     private val studySeats = mutableListOf<StudySeat>()
@@ -37,12 +46,13 @@ class DBInitializer(
         {
             this.initUsers()
             this.initSeats()
+            this.initSettings()
         }
     }
 
     private fun initUsers()
     {
-        this.logger.info("### Creating users...")
+        this.logger.info("### Creating users")
 
         this.userService.signUpUser(CreateAccount("Alfredo", "Santoro",
             "a.santoro73@studenti.unisa.it", "a.santoro73", "lamiapass", AccountType.ADMIN))
@@ -56,12 +66,12 @@ class DBInitializer(
         this.userService.signUpUser(CreateAccount("Mario", "Rossi",
             "mariorossi@tesi.it", "mrossi", "lamiapass", AccountType.USER))
 
-        this.logger.info("### Users created...")
+        this.logger.info("### Users created")
     }
 
     private fun initTagNFC(): List<TagNfc>
     {
-        this.logger.info("### Creating tag NFC...")
+        this.logger.info("### Creating tag NFC")
 
         val tagNFC = listOf(this.tagNFCService.createTagNFC(TagNfcDTO("tag1", "${UUID.randomUUID()}")),
         this.tagNFCService.createTagNFC(TagNfcDTO("tag2", "${UUID.randomUUID()}")),
@@ -76,20 +86,28 @@ class DBInitializer(
         this.tagNFCService.createTagNFC(TagNfcDTO("tag11", "${UUID.randomUUID()}")),
         this.tagNFCService.createTagNFC(TagNfcDTO("tag12", "${UUID.randomUUID()}")))
 
-        this.logger.info("### Tag NFC created...")
+        this.logger.info("### Tag NFC created")
 
         return tagNFC
     }
 
     private fun initSeats()
     {
-        this.logger.info("### Creating seats...")
+        this.logger.info("### Creating seats")
 
         this.initTagNFC().forEachIndexed { index, tagNfc ->
             this.studySeats.add(this.seatsService.createSeat(SeatInsertDTO("seat$index", true, tagNfc.id!!))!!)
         }
 
-        this.logger.info("### Seats created...")
+        this.logger.info("### Seats created")
+    }
+
+    private fun initSettings()
+    {
+        this.logger.info("### creating settings")
+        this.settingService.createSetting(SettingDTO(this.reservationHistorySetting,
+            this.days, "reservation history", ChronoUnit.DAYS))
+        this.logger.info("### Settings created")
     }
 }
 
