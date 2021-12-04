8b9aaa18238a686f7aa7025cba193751a23e5789
Alfredo Santoro <alfredosantoro.informatico@gmail.com>
2021-12-04 14:07:05 +0100

    After rebase on master

---
+++ codemr/sesalabods/html/html_report_sesalabodsmain/htmlx/modeldata/packagedependency.js
@@ -0,0 +1,71 @@
+var matrix = [[0,0,0,0,0,1,0,1,0,0,0,0,4,0,1,0,0,1,0,2,0,0,0],[0,0,0,0,0,0,0,0,0,0,0,1,2,0,0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0,0,0,0,4,0,0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0,0,0,1,2,0,0,0,0,0,0,0,0,0,0],[0,0,0,0,1,0,0,0,0,0,0,0,2,0,0,0,0,0,0,2,0,0,0],[0,0,0,1,0,0,0,0,0,0,0,1,2,0,0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0,0,0,0,2,1,0,0,0,0,0,2,0,0,0],[0,0,0,0,0,0,0,0,0,0,0,1,2,0,0,0,0,0,0,0,0,0,0],[0,0,0,0,0,1,0,0,0,0,0,0,2,0,0,0,0,0,1,2,0,0,0],[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,2,0,0,1],[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,2,0,0,0],[0,0,0,0,0,0,0,0,0,0,0,1,2,0,0,0,0,0,0,0,0,0,0],[1,0,0,0,0,0,2,1,1,1,0,0,4,0,0,0,0,0,0,2,0,0,1],[0,1,0,0,0,0,0,0,0,0,0,0,2,0,0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0,0,0,1,2,0,0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0,0,0,1,2,0,0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,2,0,0,0,2,0,0,1,0,0,0,2,0,0,0],[0,0,0,0,0,1,0,1,0,1,0,0,0,0,0,1,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0,0,0,1,2,0,0,0,0,0,0,0,0,0,0]]
+var packages = [{
+"name": " com.unisa.sesalab.ods.service.checkin", "color": " #3182bd"
+}
+,{
+"name": " com.unisa.sesalab.ods.repository.users", "color": " #6baed6"
+}
+,{
+"name": " com.unisa.sesalab.ods.factory", "color": " #9ecae1"
+}
+,{
+"name": " com.unisa.sesalab.ods.enum", "color": " #c6dbef"
+}
+,{
+"name": " com.unisa.sesalab.ods.repository.tagnfc", "color": " #e6550d"
+}
+,{
+"name": " com.unisa.sesalab.ods.service.tagnfc", "color": " #fd8d3c"
+}
+,{
+"name": " com.unisa.sesalab.ods.repository.reservations", "color": " #fdae6b"
+}
+,{
+"name": " com.unisa.sesalab.ods.service.setting", "color": " #fdd0a2"
+}
+,{
+"name": " com.unisa.sesalab.ods.repository.authorizations", "color": " #31a354"
+}
+,{
+"name": " com.unisa.sesalab.ods.service.seat", "color": " #74c476"
+}
+,{
+"name": " com.unisa.sesalab.ods.service.openingtime", "color": " #a1d99b"
+}
+,{
+"name": " com.unisa.sesalab.ods.repository", "color": " #c7e9c0"
+}
+,{
+"name": " com.unisa.sesalab.ods.model", "color": " #756bb1"
+}
+,{
+"name": " com.unisa.sesalab.ods.repository.settings", "color": " #9e9ac8"
+}
+,{
+"name": " com.unisa.sesalab.ods.service.reservation", "color": " #bcbddc"
+}
+,{
+"name": " com.unisa.sesalab.ods.service.user", "color": " #dadaeb"
+}
+,{
+"name": " com.unisa.sesalab.ods.exception", "color": " #636363"
+}
+,{
+"name": " com.unisa.sesalab.ods.repository.checkin", "color": " #969696"
+}
+,{
+"name": " com.unisa.sesalab.ods.repository.seats", "color": " #bdbdbd"
+}
+,{
+"name": " com.unisa.sesalab.ods.dto", "color": " #d9d9d9"
+}
+,{
+"name": " com.unisa.sesalab.ods.service.authorization", "color": " #3182bd"
+}
+,{
+"name": " com.unisa.sesalab.ods", "color": " #6baed6"
+}
+,{
+"name": " com.unisa.sesalab.ods.repository.openingtime", "color": " #9ecae1"
+}
+];
